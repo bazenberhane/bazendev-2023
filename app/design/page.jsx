9b2d88609b4ece1cb7c75ac9cb3a081d1d792ba0
bazencodes <109030092+bazenberhane@users.noreply.github.com>
2023-08-23 09:32:27 -0400

    basic nav, works, design, contact pages

---
+++ app/design/page.jsx
@@ -0,0 +1,7 @@
+export default function DesignPage() {
+  return (
+    <div className=''>
+      <h1>Design</h1>
+    </div>
+  )
+}
---
+++ app/works/page.jsx
@@ -0,0 +1,7 @@
+export default function WorksPage() {
+  return (
+    <div className=''>
+      <h1>Works</h1>
+    </div>
+  )
+}
